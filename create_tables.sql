c974536b81b6075817669b68b343e1c919143806
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2024-03-28 21:02:25 +0530

    Add foreign key constraints and create tables for spaceship management

--- create_tables.sql
+++ create_tables.sql
@@ -1,22 +1,30 @@
-CREATE DATABASE spaceship_management;
-CREATE TABLE launch (
+CREATE DATABASE if NOT EXISTS spaceship_management;
+
+Use spaceship_management;
+
+CREATE TABLE if NOT EXISTS launch (
     launch_id VARCHAR(10) NOT NULL PRIMARY KEY,
+    launchsite_id varchar(10) not null default "0",
+    payload_id varchar(10) not null default "0",
+    launchvehicle_id varchar(10) not null default "0",
     date DATE,
     time TIME
 );
-CREATE TABLE payload (
+CREATE TABLE if NOT EXISTS payload (
     payload_id VARCHAR(10) NOT NULL PRIMARY KEY,
     owner VARCHAR(100)
 );
-CREATE TABLE spacecraft (
+CREATE TABLE if NOT EXISTS spacecraft (
     spacecraft_id VARCHAR(20) NOT NULL PRIMARY KEY,
+    payload_id varchar(10) not null default "0",
     crew_capacity VARCHAR(10)
 );
-CREATE TABLE satelite (
+CREATE TABLE if NOT EXISTS satelite (
     satelite_id VARCHAR(10) NOT NULL PRIMARY KEY,
+    payload_id varchar(10) not null default "0",
     orbit VARCHAR(50)
 );
-CREATE TABLE launchvehicle (
+CREATE TABLE if NOT EXISTS launchvehicle (
     launchvehicle_id VARCHAR(10) NOT NULL PRIMARY KEY,
     manufacture VARCHAR(50),
     payload_capacity INT,
@@ -24,14 +32,16 @@ CREATE TABLE launchvehicle (
     capsule VARCHAR(20),
     rocket VARCHAR(20)
 );
-CREATE TABLE launchsite (
+CREATE TABLE if NOT EXISTS launchsite (
     launchsite_id VARCHAR(10) NOT NULL PRIMARY KEY,
     atitude FLOAT,
     longitude FLOAT
 );
-CREATE TABLE launchProvider (
+CREATE TABLE if NOT EXISTS launchProvider (
     launchProvider_id VARCHAR(10) NOT NULL PRIMARY KEY,
     county_of_origin VARCHAR(50),
     website VARCHAR(100)
 );
-	
+
+
+
